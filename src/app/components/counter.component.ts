3c6f961c8b779a29bfec9173276477244758ea51
home <home@homes-imac.attlocal.net>
2016-09-03 22:37:57 -0700

    added simple template to app-component

--- src/app/components/counter.component.ts
+++ src/app/components/counter.component.ts
@@ -4,7 +4,11 @@ import { Store } from '@ngrx/store';
 import { Observable } from 'rxjs/Observable';
 
 @Component({
-	selector: 'counter-component'
+	selector: 'counter-component',
+	template: `
+		<h2>Sweet Counter Thing:</h2>
+		<h3>Current Value: {{counter$}}</h3>
+	`
 })
 export class CounterComponent{
 	counter$:Observable<number>;
