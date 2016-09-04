f30e65753136d7a97a1c923ad3d58a1000dddb83
home <home@homes-imac.attlocal.net>
2016-09-03 23:19:32 -0700

    counter works
    don’t understand async pipe, need to check out

--- src/app/components/counter.component.ts
+++ src/app/components/counter.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ChangeDetectionStrategy } from '@angular/core';
 // @Ngrx stuff
 import { Store } from '@ngrx/store';
 import { Observable } from 'rxjs/Observable';
@@ -7,17 +7,39 @@ import { Observable } from 'rxjs/Observable';
 	selector: 'counter-component',
 	template: `
 		<h2>Sweet Counter Thing:</h2>
-		<h3>Current Value: {{counter$}}</h3>
-	`
+		<button (click)="increment()">+</button>
+        <button (click)="decrement()">-</button>
+        <h3>{{counter$ | async}}</h3>
+	`,
+	changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class CounterComponent{
-	counter$:Observable<number>;
 
-	constructor(
-		private store: Store<number>
-	){
-		this.counter$ = this.store.select<number>('counter');
-		console.log(this.counter$);
-	}
-	
+export class CounterComponent{
+    counter$: Observable<number>;
+
+    constructor(
+        private store : Store<number>
+    ){
+        /*
+            Select returns an observable of the appropriate slice of state (reducer) from store.
+            This is equivalent to store.map(state => state['counter']).distinctUntilChanged()
+         */
+        this.counter$ = this.store.select<number>('counter');
+    }
+    /*
+        The only way to modify state in store is through dispatched actions.
+        Actions require a type (string) and optional payload.
+        This type will match up to a case in one of your application reducers,
+        specifying how this action will create a new representation
+        of that particular section of state.
+     */
+    increment(){
+        this.store.dispatch({type: 'INCREMENT'});
+    }
+
+    decrement(){
+        this.store.dispatch({type: 'DECREMENT'});
+    }
+
+    
 }
--- src/app/reducers/counter.ts
+++ src/app/reducers/counter.ts
@@ -7,10 +7,12 @@ import { ActionReducer, Action } from '@ngrx/store';
 */
 
 export const counter: ActionReducer<number> = (state: number = 0, action: Action) => {
-	switch(action){
-
-		default:
-			// console.log(this);
-			return state;
-	}
-}; 
+    switch(action.type){
+        case 'INCREMENT':
+            return state + 1;
+        case 'DECREMENT':
+            return state - 1;
+        default:
+            return state;
+    }
+};
