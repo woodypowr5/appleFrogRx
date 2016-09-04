fa8cf870ab90ecd963d60b2bf7d041b158d7a901
home <home@homes-imac.attlocal.net>
2016-09-03 21:50:30 -0700

    created counter reducer with default state

---
+++ src/app/reducers/counter.ts
@@ -0,0 +1,14 @@
+import { ActionReducer, Action } from '@ngrx/store';
+
+/*
+    Default parameter will be used for initial state unless initial
+    state is provided for this reducer in 'provideStore' method.
+*/
+
+export const counter: ActionReducer<number> = (state: number = 0, action: Action) => {
+	switch(action){
+
+		default:
+			return state;
+	}
+}; 
