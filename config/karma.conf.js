a3518c75ed30254a83b30801b8a223feb2db01be
home <home@homes-imac.attlocal.net>
2016-09-05 04:45:55 -0700

    added karma mocha reporter

--- config/karma.conf.js
+++ config/karma.conf.js
@@ -38,7 +38,7 @@ module.exports = function (config) {
     reporters: ['progress'],
     port: 9876,
     colors: true,
-    logLevel: config.LOG_INFO,
+    logLevel: config.LOG_DEBUG,
     autoWatch: true,
     browsers: ['Chrome'],
     singleRun: false
--- src/app/reducers/counter.spec.ts
+++ src/app/reducers/counter.spec.ts
@@ -1,3 +1,23 @@
-import 'rxjs/add/operator/let';
-import { of } from 'rxjs/observable/of';
+import {
+  it,
+  inject,
+  async,
+  describe,
+  expect,
+  beforeEachProviders
+} from '@angular/core/testing';
 
+import { counter } from './counter';
+import { counterActions } from './counter.actions';
+
+describe('The Counter Reducer', () => {
+    const mockedState = {
+      	value: 0
+    };
+    it('should return current state when no valid actions have been made', () => {
+        const state = Object.assign({}, mockedState);
+        const actual = counter(state, {type: 'INVALID_ACTION'});
+        const expected = state;
+        expect(actual).toBe(expected);
+    });
+});    
