1fa5b7ae90988e80837018d96662c55c770b15f4
home <cwoodward5@hotmail.com>
2016-09-11 20:34:33 -0700

    default action returns current state

--- src/app/store/profile/profile.actions.ts
+++ src/app/store/profile/profile.actions.ts
@@ -10,7 +10,7 @@ export class profileActions {
   //     payload: newProfile
   //   }
   // }
- 
+  
 }
 
 
--- src/app/store/profile/profile.spec.ts
+++ src/app/store/profile/profile.spec.ts
@@ -11,15 +11,15 @@ import { profileReducer } from './profile.reducer';
 import { profileActions } from './profile.actions';
 
 describe('The Profile Reducer', () => {
-    // const mockedState = {
-    //   	value: 0
-    // };
-    // it('should return current state when no valid actions have been made', () => {
-    //     const state = Object.assign({}, mockedState);
-    //     const actual = profileReducer(state, {type: 'INVALID_ACTION'});
-    //     const expected = state;
-    //     expect(actual).toBe(expected);
-    // });
+    const mockedState = {
+      	value: 0
+    };
+    it('should return current state when no valid actions have been made', () => {
+        const state = Object.assign({}, mockedState);
+        const actual = profileReducer(state, {type: 'INVALID_ACTION'});
+        const expected = state;
+        expect(actual).toBe(expected);
+    });
     // it('should add 1 to state.value when INCREMENT action is dispatched', () => {
     //     const state = Object.assign({}, mockedState);
     //     const actual = profileReducer(state, {type: profileActions.INCREMENT});
